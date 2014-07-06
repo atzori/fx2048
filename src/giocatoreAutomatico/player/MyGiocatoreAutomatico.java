d67690206fb201ff5a9247118e913cbdf49f8bac
Andrea <Andrea@AndreaPC>
2014-07-06 21:18:59 +0200

    Aggiunta del giocatore automatico stupido

--- src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
+++ src/giocatoreAutomatico/player/MyGiocatoreAutomatico.java
@@ -5,15 +5,27 @@ import giocatoreAutomatico.Griglia;
 import java.util.Random;
 
 /**
- *
  * @author Andrea
+ * @date 06/07/2014
+ * Questa classe si occupa di far scegliere la prossima mossa al giocatore automatico.
  */
+
 public class MyGiocatoreAutomatico implements GiocatoreAutomatico {
+        
+        /***
+         * @parame g Griglia passata per consentire la prossima mossa;
+         * @parame numeroCasuale numero casuale per consentire la scelta;
+         * @parame scelta numero che rappresenta la scelta del giocatore automatico;
+         * @return scelta numero che rappresenta la scelta del giocatore automatico
+         */
+    
         @Override
         public int prossimaMossa(Griglia g){
+            
             Random numeroCasuale= new Random();
-            numeroCasuale.setSeed(4);
+            numeroCasuale.setSeed(0);
+            int scelta=(numeroCasuale.nextInt())%4;
 
-            return numeroCasuale.nextInt();
+            return scelta;
         }
 }
